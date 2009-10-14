56e1781ba41685f67e04f8761e3e787fb1612a6f
Martin Fitzpatrick <martin.fitzpatrick@spenglr.org>
2009-10-14 01:00:42 +0100

    Institutions are now 'networks' to be more general purpose.

---
+++ education/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.admin import widgets                                       
+from spenglr.education.models import UserCourse
+
+class UserCourseRegisterForm(forms.ModelForm):
+    class Meta:
+        model = UserCourse
+    def __init__(self, *args, **kwargs):
+        super(UserCourseRegisterForm, self).__init__(*args, **kwargs)
+        self.fields['start_date'].widget = widgets.AdminDateWidget()
+    start_date = forms.DateField(required=True)
+
---
+++ urls.py
@@ -0,0 +1,37 @@
+from django.conf.urls.defaults import *
+from django.conf.urls.defaults import *
+from django.contrib import admin
+admin.autodiscover()
+
+def i18n_javascript(request):
+  return admin.site.i18n_javascript(request)
+
+urlpatterns = patterns('',
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+
+    (r'^admin/jsi18n', i18n_javascript),
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+    (r'^$', 'spenglr.core.views.index'),
+    #(r'^login/$', 'spenglr.core.views.loginhandler'),
+    #(r'^logout/$', 'spenglr.core.views.logouthandler'),
+
+    (r'^education/', include('spenglr.education.urls')),
+
+    (r'^network/', include('spenglr.network.urls')),
+    
+    (r'^accounts/', include('registration.urls')),
+
+    # These exist so we can keep a standardised url forum across all 3
+    # as the m/i/c is not actually used on the forum code (can't have / in slugs)
+    (r'^discuss/m/', include('forum.urls')),
+    (r'^discuss/i/', include('forum.urls')),
+    (r'^discuss/c/', include('forum.urls')),
+
+    (r'^questions/', include('spenglr.questions.urls')),
+    #(r'^resources/', include('spenglrcom.resources.urls')),
+
+
+)
+
