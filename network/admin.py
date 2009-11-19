62a0c2f6fd236c9eef6c4b4a99bdde28a93521e2
Martin Fitzpatrick <martin.fitzpatrick@spenglr.org>
2009-11-19 01:53:53 +0000

    Network moved back out (yawn) to keep it tidy/logical and make the paths nicer.

---
+++ network/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from spenglr.education.models import *
+
+
+admin.site.register(Network)
+
+admin.site.register(UserNetwork)
---
+++ network/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.admin import widgets                                       
+from spenglr.education.models import UserNetwork
+
+class UserNetworkRegisterForm(forms.ModelForm):
+    class Meta:
+        model = UserNetwork
+    def __init__(self, *args, **kwargs):
+        super(UserNetworkRegisterForm, self).__init__(*args, **kwargs)
