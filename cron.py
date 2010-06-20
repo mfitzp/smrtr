c035c77c715c59bf2c8477f7edb6e111b8ed0d2e
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2010-06-20 19:16:20 +0100

    Crontab seconds output, messages

--- cron.py
+++ cron.py
@@ -10,13 +10,13 @@ from profiles.utils import *
 from network.utils import *
 
 def cron():
-    print datetime.now().strftime('%H:%M') + ": Update SQ values..."
+    print datetime.now().strftime('%H:%M:%S') + ": Update SQ values..."
     batch_question_update_sq()
     
     batch_module_update_sq()
     batch_concept_update_sq()    
 
-    print datetime.now().strftime('%H:%M') + ": Update user SQ values..."
+    print datetime.now().strftime('%H:%M:%S') + ": Update user SQ values..."
 
     batch_usermodule_update_sq()
     batch_userconcept_update_sq()
@@ -25,7 +25,7 @@ def cron():
     batch_user_update_sq()
     batch_network_update_sq()
 
-    print datetime.now().strftime('%H:%M') + ": Generating challenges..."
+    print datetime.now().strftime('%H:%M:%S') + ": Generating challenges..."
     batch_generate_user_challenges()
 
     print datetime.now().strftime('%A %d/%m/%Y %H:%M') + ": Done."
