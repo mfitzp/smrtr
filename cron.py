2825498c0f691525eb69b29d72bdda031f7b6abc
Martin Fitzpatrick <martin.fitzpatrick@spenglr.org>
2010-07-25 19:13:42 +0100

    Cron reactivated

--- cron.py
+++ cron.py
@@ -11,22 +11,22 @@ from network.utils import *
 
 def cron():
     print datetime.now().strftime(' %H:%M:%S') + ": Update question SQ..."
-    #batch_question_update_sq()
+    batch_question_update_sq()
     
     print datetime.now().strftime(' %H:%M:%S') + ": Update network, module & concept SQ..."
-    #batch_module_update_sq()
-    #batch_concept_update_sq()    
-    #batch_network_update_sq()
+    batch_module_update_sq()
+    batch_concept_update_sq()    
+    batch_network_update_sq()
 
     print datetime.now().strftime(' %H:%M:%S') + ": Update user profile SQ..."
-    #batch_user_update_sq()
+    batch_user_update_sq()
 
     print datetime.now().strftime(' %H:%M:%S') + ": Update user module & concept SQ..."
-    #batch_usermodule_update_sq()
-    #batch_userconcept_update_sq()
+    batch_usermodule_update_sq()
+    batch_userconcept_update_sq()
 
     print datetime.now().strftime(' %H:%M:%S') + ": Update user cocept focus..."
-    #batch_userconcept_update_focus()
+    batch_userconcept_update_focus()
 
     print datetime.now().strftime(' %H:%M:%S') + ": Generating challenges..."
     batch_generate_user_challenges()
