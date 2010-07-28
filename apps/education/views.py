6999ea6a172a2ca84b4403da5fb3d424cf0468b1
Martin Fitzpatrick <martin.fitzpatrick@spenglr.org>
2010-07-28 17:26:40 +0100

    Pagination (concept_add_questions) bugfix

--- apps/education/views.py
+++ apps/education/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.utils.translation import ugettext as _
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator, InvalidPage
 # Spenglr
 from education.models import *
 from network.models import *
