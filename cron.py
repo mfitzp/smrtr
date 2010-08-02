34657258c3c3a0e1c28c162a7b08825368d721d1
Martin Fitzpatrick <martin.fitzpatrick@spenglr.org>
2010-08-02 03:03:39 +0100

    cron.py restore

--- cron.py
+++ cron.py
@@ -11,27 +11,27 @@ from network.utils import *
 
 def cron():
     print datetime.datetime.now().strftime(' %H:%M:%S') + ": Update question SQ/ttc..."
-    #batch_question_update_sq()
-    #batch_question_update_ttc()
+    batch_question_update_sq()
+    batch_question_update_ttc()
     
     print datetime.datetime.now().strftime(' %H:%M:%S') + ": Update network, module & concept SQ..."
-    #batch_module_update_sq()
-    #batch_concept_update_sq()    
-    #batch_network_update_sq()
+    batch_module_update_sq()
+    batch_concept_update_sq()    
+    batch_network_update_sq()
 
     print datetime.datetime.now().strftime(' %H:%M:%S') + ": Update user profile SQ..."
-    #batch_user_update_sq()
+    batch_user_update_sq()
     batch_user_normalise_sq()
 
     print datetime.datetime.now().strftime(' %H:%M:%S') + ": Update user module & concept SQ..."
-    #batch_usermodule_update_sq()
-    #batch_userconcept_update_sq()
+    batch_usermodule_update_sq()
+    batch_userconcept_update_sq()
 
     print datetime.datetime.now().strftime(' %H:%M:%S') + ": Update user concept focus..."
-    #batch_userconcept_update_focus()
+    batch_userconcept_update_focus()
 
     print datetime.datetime.now().strftime(' %H:%M:%S') + ": Generating challenges..."
-    #batch_generate_user_challenges()
+    batch_generate_user_challenges()
 
     print datetime.datetime.now().strftime(' %H:%M:%S') + ": Done."
 cron()
