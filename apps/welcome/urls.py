240d23bb1c05f541cd81fc569209b8b503fae22c
Martin Fitzpatrick <martin.fitzpatrick@spenglr.org>
2010-08-16 20:47:17 +0100

    Reduce noise on dashboard, now showing challenge listing only. Linkthrough to activate modules. Progress is on profile page

--- apps/welcome/urls.py
+++ apps/welcome/urls.py
@@ -10,7 +10,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
 
     url(r'^1$', 'welcome.views.profile', name='welcome-1' ),
-    url(r'^2$', 'network.views.search', {'next': '3'}, name='welcome-2' ),
-    url(r'^3$', 'education.views.module_search', {'next': '/'}, name='welcome-3' )
+    url(r'^2$', 'network.views.search', {'next': '/'}, name='welcome-2' ),
+    
+    # Skip this so user gets the welcome message and instruction (keeps it easy to follow)
+    #url(r'^3$', 'education.views.module_search', {'next': '/'}, name='welcome-3' )
 
 )
