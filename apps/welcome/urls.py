c9587decdb67474959e1957378f6a4987e4c320a
Martin Fitzpatrick <martin.fitzpatrick@spenglr.org>
2010-08-16 19:27:36 +0100

    Add select modules to the welcome steps. Redirect fixes to skip

--- apps/welcome/urls.py
+++ apps/welcome/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.core.urlresolvers import reverse
 # Smrtr
 
 # Uncomment the next two lines to enable the admin:
@@ -9,7 +10,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
 
     url(r'^1$', 'welcome.views.profile', name='welcome-1' ),
-    url(r'^2$', 'network.views.search', {'next': 'home'}, name='welcome-2' )
-    #url(r'^3$', 'welcome.views.profile', name='welcome-1' ),
+    url(r'^2$', 'network.views.search', {'next': '3'}, name='welcome-2' ),
+    url(r'^3$', 'education.views.module_search', {'next': '/'}, name='welcome-3' )
 
 )
