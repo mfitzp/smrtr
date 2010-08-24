b6427ab06867ddf93c1314b13d95365fd72c9d6a
Martin Fitzpatrick <martin.fitzpatrick@spenglr.org>
2010-08-24 20:12:10 +0100

    ...

--- apps/challenge/models.py
+++ apps/challenge/models.py
@@ -125,8 +125,8 @@ class UserChallenge(models.Model):
                         self.end_date = datetime.datetime.now()
                 
                         # Are we first?
-                        first = self.challenge.userchallenge_set.filter(self.percent_complete=100).order_by('completed')[0]:
-                        if self.user == first.user 
+                        first = self.challenge.userchallenge_set.filter(percent_complete=100).order_by('completed')[0]
+                        if self.user == first.user:
                             add_extended_wallitem( self.challenge.wall, self.user, template_name='challenge_1stcomplete.html', extra_context={'challenge': self.challenge, 'userchallenge': self, })
 
                         # Did we ace it?
