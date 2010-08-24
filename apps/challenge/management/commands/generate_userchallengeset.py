2eaaf360139901edf609498ad24c8c6bf9affca4
Martin Fitzpatrick <martin.fitzpatrick@spenglr.org>
2010-08-24 20:47:18 +0100

    ...

---
+++ apps/challenge/management/commands/generate_userchallengeset.py
@@ -0,0 +1,13 @@
+from optparse import make_option
+import sys
+from django.core.management.base import BaseCommand
+from challenge.models import UserChallenge
+
+class Command(BaseCommand):
+    args = ""
+    help = "Update concept total_question counts (post db import)"
+    
+    def handle(self, *args, **options):
+        for userchallenge in UserChallenge.objects.filter(challengeset_id=0):
+            userchallenge.generate_challengeset()
+
