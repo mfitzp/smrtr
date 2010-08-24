ca2a861c3e9d46893af05ea5df7bf6551947a8b7
Martin Fitzpatrick <martin.fitzpatrick@spenglr.org>
2010-08-24 22:05:57 +0100

    ...

--- apps/challenge/management/commands/fix_userchallenge_end_dates.py
+++ apps/challenge/management/commands/fix_userchallenge_end_dates.py
@@ -1,8 +1,10 @@
 from optparse import make_option
 import sys
 from django.core.management.base import BaseCommand
-from challenge.models import ChallengeSet,UserChallengeSet,UserChallenge
 from django.db.models import Avg, Max, Min, Count, Sum
+# Smrtr
+from challenge.models import ChallengeSet,UserChallengeSet,UserChallenge
+from questions.models import UserQuestionAttempt
 
 class Command(BaseCommand):
     args = ""
@@ -11,8 +13,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for userchallenge in UserChallenge.objects.exclude(end_date=None):
             o = UserChallengeSet.objects.filter(user=userchallenge.user, challengeset__challenge=userchallenge.challenge).exclude(completed=None).aggregate(Max('completed'))
-            print o
             if o:
                 if o['completed__max'] is not None:
                     userchallenge.end_date = o['completed__max']
+                    userchallenge.save()
+                else:
+                    # Get from latest question
+                    o = UserQuestionAttempt.objects.filter(user=userchallenge.user, question__concepts__challenge=userchallenge.challenge).aggregate(Max('created'))
+                    userchallenge.end_date = o['created__max']
                     userchallenge.save()
